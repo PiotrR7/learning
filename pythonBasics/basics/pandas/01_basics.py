0285c8558b321fa06f6d2e0d07544fadb1f3b459
PiotrR7 <p.rosiek7@gmail.com>
2026-03-30 22:23:27 +0200

    Python pandas 30.03.2026

---
+++ pythonBasics/basics/pandas/01_basics.py
@@ -0,0 +1,7 @@
+"""
+    pandas jest biblioteką do pracy nad danymi
+"""
+
+import pandas as pd
+
+dataFrame = pd.read_csv("https://raw.githubusercontent.com/zief0002/epsy-5261/main/data/countries.csv")
